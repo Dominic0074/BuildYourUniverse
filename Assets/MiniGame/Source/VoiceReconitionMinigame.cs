64be87ed31423c07593aac560ae2e47fdf682f5e
Lissyflecky <65901790+Lissyflecky@users.noreply.github.com>
2022-07-06 16:43:54 +0200

    VoiceReconition navigaton from games to universe

---
+++ Assets/MiniGame/Source/VoiceReconitionMinigame.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
+
+public class VoiceReconitionMinigame : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+
+        if (args.text == m_Keywords[0])
+        {
+            SceneManager.LoadScene("OpenWorld");
+        }
+    }
+}
